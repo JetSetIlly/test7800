JetSetIlly/test7800

commit 5a34270f074581a5751e2f0a3edbe5b84b6c32b6

lineram is read clock-by-clock (but still written at once at start of DMA)

hardware/maria/maria.go
@@ -4,51 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"image"
-	"image/color"
 	"strings"
 	"time"
 
 	"github.com/jetsetilly/test7800/hardware/clocks"
 	"github.com/jetsetilly/test7800/ui"
 )
-
-type mariaCtrl struct {
-	colourKill bool
-	dma        int // 0 to 3 only
-	charWidth  bool
-	border     bool
-	kanagroo   bool
-	readMode   int // 0 to 3 only
-}
-
-func (ctrl *mariaCtrl) reset() {
-	ctrl.colourKill = false
-	ctrl.dma = 3
-	ctrl.charWidth = false
-	ctrl.border = false
-	ctrl.kanagroo = false
-	ctrl.readMode = 0
-}
-
-func (ctrl *mariaCtrl) write(data uint8) {
-	ctrl.colourKill = data&0x80 == 0x80
-	ctrl.dma = int((data >> 5) & 0x03)
-	ctrl.charWidth = data&0x10 == 0x10
-	ctrl.border = data&0x08 == 0x08
-	ctrl.kanagroo = data&0x04 == 0x04
-	ctrl.readMode = int(data & 0x03)
-}
-
-func (ctrl *mariaCtrl) String() string {
-	var s strings.Builder
-	s.WriteString(fmt.Sprintf("ck=%v ", ctrl.colourKill))
-	s.WriteString(fmt.Sprintf("dma=%#02b ", ctrl.dma))
-	s.WriteString(fmt.Sprintf("cw=%v ", ctrl.charWidth))
-	s.WriteString(fmt.Sprintf("bc=%v ", ctrl.border))
-	s.WriteString(fmt.Sprintf("km=%v ", ctrl.kanagroo))
-	s.WriteString(fmt.Sprintf("rm=%#02b ", ctrl.readMode))
-	return s.String()
-}
 
 // Context allows Maria to signal a break
 type Context interface {
@@ -101,10 +62,7 @@ type Maria struct {
 	// pixels for current frame
 	currentFrame *image.RGBA
 
-	// lineram is implemented as a single line image. this isn't an exact
-	// representation of lineram but it's workable and produces adequate results
-	// for now
-	lineram *image.RGBA
+	lineram lineram
 
 	// the current spec (NTSC, PAL, etc.)
 	spec spec
@@ -142,8 +100,7 @@ func Create(ctx Context, ui *ui.UI, mem Memory) *Maria {
 	// TODO: better interaction between frame limiter and audio
 	mar.limiter = time.NewTicker(time.Second / time.Duration(hz))
 
-	// allocate images representing lineram and the frame to be displayed
-	mar.lineram = image.NewRGBA(image.Rect(0, 0, clksVisible, 1))
+	mar.lineram.initialise()
 	mar.newFrame()
 
 	return mar
@@ -376,12 +333,6 @@ const (
 
 func (mar *Maria) newFrame() {
 	mar.currentFrame = image.NewRGBA(image.Rect(0, 0, clksVisible, mar.spec.visibleBottom-mar.spec.visibleTop))
-
-	// make sure lineram is clear at beginning of new frame. probably not
-	// necessary but not an expensive operation
-	for clk := range clksVisible {
-		mar.lineram.Set(clk, 0, color.Transparent)
-	}
 }
 
 // returns true if CPU is to be halted and true if DLL has requested an interrupt
@@ -392,6 +343,7 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 		mar.Coords.Scanline++
 		mar.wsync = false
 		mar.requiredDMACycles = 0
+		mar.lineram.newScanline()
 
 		if mar.Coords.Scanline >= mar.spec.absoluteBottom {
 			mar.Coords.Scanline = 0
@@ -422,6 +374,22 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 		}
 	}
 
+	// read from lineram and draw to screen
+	if mar.Coords.Clk > clksHBLANK {
+		sl := mar.Coords.Scanline - mar.spec.visibleTop
+		x := mar.Coords.Clk - clksHBLANK
+		e := mar.lineram.read(x)
+		if e.set {
+			if e.idx == bgIdx {
+				mar.currentFrame.Set(x, sl, mar.spec.palette[mar.bg])
+			} else {
+				mar.currentFrame.Set(x, sl, mar.spec.palette[mar.palette[e.palette][e.idx]])
+			}
+		} else {
+			mar.currentFrame.Set(x, sl, mar.spec.palette[mar.bg])
+		}
+	}
+
 	// scanline build is done at the start of DMA
 	if mar.Coords.Clk == preDMA {
 		// DMA is only ever active when VBLANK is disabled
@@ -444,23 +412,6 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 					mar.requiredDMACycles += dmaStartLastInZone
 				} else {
 					mar.requiredDMACycles += dmaStart
-				}
-
-				// the scanline value adjusted by where the top of the image is located
-				sl := mar.Coords.Scanline - mar.spec.visibleTop
-
-				for clk := range clksVisible {
-					// set entire scanline to background colour
-					mar.currentFrame.Set(clk, sl, mar.spec.palette[mar.bg])
-
-					// copy line ram over background where the line ram is not transparent
-					c := mar.lineram.RGBAAt(clk, 0)
-					if c.A != 0 {
-						mar.currentFrame.Set(clk, sl, c)
-					}
-
-					// clear the part of line ram we just copied
-					mar.lineram.Set(clk, 0, color.Transparent)
 				}
 
 				err = mar.nextDL(true)
@@ -518,31 +469,33 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 									// 160B
 									for i := range 2 {
 										c := (b >> (((1 - i) * 2) + 4)) & 0x03
-										pi := (mar.DL.palette & 0x40) + ((b >> ((1 - i) * 2)) & 0x03)
-										p := mar.palette[pi]
+										p := (mar.DL.palette & 0x40) + ((b >> ((1 - i) * 2)) & 0x03)
 										x := int(mar.DL.horizontalPosition + (offset * 2))
 										x = (x + i) * 2
-										if c > 0 {
-											mar.lineram.Set(x, 0, mar.spec.palette[p[c-1]])
-											mar.lineram.Set(x+1, 0, mar.spec.palette[p[c-1]])
-										} else if mar.ctrl.kanagroo {
-											mar.lineram.Set(x, 0, mar.spec.palette[mar.bg])
-											mar.lineram.Set(x+1, 0, mar.spec.palette[mar.bg])
+										if x < clksVisible {
+											if c > 0 {
+												mar.lineram.write(x, p, c-1)
+												mar.lineram.write(x+1, p, c-1)
+											} else if mar.ctrl.kangaroo {
+												mar.lineram.write(x, p, bgIdx)
+												mar.lineram.write(x+1, p, bgIdx)
+											}
 										}
 									}
 								} else {
 									// 160A
-									p := mar.palette[mar.DL.palette]
 									for i := range 4 {
 										c := (b >> ((3 - i) * 2)) & 0x03
 										x := int(mar.DL.horizontalPosition + (offset * 4))
 										x = (x + i) * 2
-										if c > 0 {
-											mar.lineram.Set(x, 0, mar.spec.palette[p[c-1]])
-											mar.lineram.Set(x+1, 0, mar.spec.palette[p[c-1]])
-										} else if mar.ctrl.kanagroo {
-											mar.lineram.Set(x, 0, mar.spec.palette[mar.bg])
-											mar.lineram.Set(x+1, 0, mar.spec.palette[mar.bg])
+										if x < clksVisible {
+											if c > 0 {
+												mar.lineram.write(x, mar.DL.palette, c-1)
+												mar.lineram.write(x+1, mar.DL.palette, c-1)
+											} else if mar.ctrl.kangaroo {
+												mar.lineram.write(x, mar.DL.palette, bgIdx)
+												mar.lineram.write(x+1, mar.DL.palette, bgIdx)
+											}
 										}
 									}
 								}
@@ -559,17 +512,18 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 									// 320C
 								} else {
 									// 320A
-									p := mar.palette[mar.DL.palette]
 									for i := range 8 {
 										// note that the horizontal position for 320 modes are doubled by the Maria
 										// when writing to line ram. this gives an effective positioning resolution
 										// of 160 pixels
 										x := int(mar.DL.horizontalPosition + (offset * 4))
 										x = x*2 + i
-										if ((b << i) & 0x80) != 0 {
-											mar.lineram.Set(x, 0, mar.spec.palette[p[1]])
-										} else if mar.ctrl.kanagroo {
-											mar.lineram.Set(x, 0, mar.spec.palette[mar.bg])
+										if x < clksVisible {
+											if (b<<i)&0x80 == 0x80 {
+												mar.lineram.write(x, mar.DL.palette, 1)
+											} else if mar.ctrl.kangaroo {
+												mar.lineram.write(x, mar.DL.palette, bgIdx)
+											}
 										}
 									}
 								}

hardware/riot/riot.go
@@ -44,7 +44,7 @@ func (riot *RIOT) Read(idx uint16) (uint8, error) {
 	case 0x02:
 		// SWCHB
 		// pro on by default (amateur would be 0x3f)
-		return 0xff, nil
+		return 0x3f, nil
 	case 0x03:
 		// SWBCNT
 		return 0, nil

hardware/maria/ctrl.go
@@ -1,0 +1,44 @@
+package maria
+
+import (
+	"fmt"
+	"strings"
+)
+
+type mariaCtrl struct {
+	colourKill bool
+	dma        int // 0 to 3 only
+	charWidth  bool
+	border     bool
+	kangaroo   bool
+	readMode   int // 0 to 3 only
+}
+
+func (ctrl *mariaCtrl) reset() {
+	ctrl.colourKill = false
+	ctrl.dma = 3
+	ctrl.charWidth = false
+	ctrl.border = false
+	ctrl.kangaroo = false
+	ctrl.readMode = 0
+}
+
+func (ctrl *mariaCtrl) write(data uint8) {
+	ctrl.colourKill = data&0x80 == 0x80
+	ctrl.dma = int((data >> 5) & 0x03)
+	ctrl.charWidth = data&0x10 == 0x10
+	ctrl.border = data&0x08 == 0x08
+	ctrl.kangaroo = data&0x04 == 0x04
+	ctrl.readMode = int(data & 0x03)
+}
+
+func (ctrl *mariaCtrl) String() string {
+	var s strings.Builder
+	s.WriteString(fmt.Sprintf("ck=%v ", ctrl.colourKill))
+	s.WriteString(fmt.Sprintf("dma=%#02b ", ctrl.dma))
+	s.WriteString(fmt.Sprintf("cw=%v ", ctrl.charWidth))
+	s.WriteString(fmt.Sprintf("bc=%v ", ctrl.border))
+	s.WriteString(fmt.Sprintf("km=%v ", ctrl.kangaroo))
+	s.WriteString(fmt.Sprintf("rm=%#02b ", ctrl.readMode))
+	return s.String()
+}

hardware/maria/lineram.go
@@ -1,0 +1,51 @@
+package maria
+
+type lineram struct {
+	// lineram is implemented as a single line image. this isn't an exact
+	// representation of lineram but it's workable and produces adequate results
+	// for now
+	lineram [2][clksVisible]lineEntry
+
+	// the index into the lineram. the values should be 0 or 1 and not equal
+	lineramRead  int
+	lineramWrite int
+}
+
+func (l *lineram) initialise() {
+	l.lineramRead = 0
+	l.lineramWrite = 1
+}
+
+func (l *lineram) newScanline() {
+	l.lineramRead, l.lineramWrite = l.lineramWrite, l.lineramRead
+	for s := range clksVisible {
+		l.lineram[l.lineramWrite][s].set = false
+	}
+}
+
+func (l *lineram) read(x int) lineEntry {
+	return l.lineram[l.lineramRead][x]
+}
+
+func (l *lineram) write(x int, palette uint8, idx uint8) {
+	l.lineram[l.lineramWrite][x].set = true
+	l.lineram[l.lineramWrite][x].palette = palette
+	l.lineram[l.lineramWrite][x].idx = idx
+}
+
+// each entry in lineram is a lineEntry instance
+type lineEntry struct {
+	// whether this entry is set or if the background should be shown
+	set bool
+
+	// which maria palette to index the colour from. values 0 to 7
+	palette uint8
+
+	// which entry in the specified palette to use. values 0 to 3
+	idx uint8
+}
+
+// the index value to use in the lineEntry struct to indicate that the colour of
+// the lineram entry should be the currently selected background colour. this
+// happens when kangaroo mode is enabled
+const bgIdx = 255

commit a001402cc554283a25172e5e92788f75dc864859

corrected errors introduced in 5a34270f
lineram read from clksHBLANK and not clksHBLANK+1

SWCHB defaults to pro again

hardware/maria/maria.go
@@ -415,8 +415,8 @@ func (mar *Maria) Tick() (halt bool, interrupt bool) {
 	sl := mar.Coords.Scanline - mar.currentFrame.top
 	x := mar.Coords.Clk - mar.currentFrame.left
 
-	// read from lineram and draw to screen
-	if mar.Coords.Clk > clksHBLANK {
+	// read from lineram and draw to screen on a clock-by-clock basis
+	if mar.Coords.Clk >= clksHBLANK {
 		e := mar.lineram.read(mar.Coords.Clk - clksHBLANK)
 
 		if e.set {

hardware/riot/riot.go
@@ -44,7 +44,7 @@ func (riot *RIOT) Read(idx uint16) (uint8, error) {
 	case 0x02:
 		// SWCHB
 		// pro on by default (amateur would be 0x3f)
-		return 0x3f, nil
+		return 0xff, nil
 	case 0x03:
 		// SWBCNT
 		return 0, nil