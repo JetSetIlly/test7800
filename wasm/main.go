c96b958a61f5ca6654d9859f7f12e5cbb766afe0
JetSetIlly <stephen.t.illingworth@gmail.com>
2025-05-20 10:56:15 +0100

    fixed wasm build by implementing context interface

--- wasm/main.go
+++ wasm/main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
-	"github.com/jetsetilly/test7800/debugger/dbg"
+	"math/rand/v2"
+
 	"github.com/jetsetilly/test7800/gui"
 	"github.com/jetsetilly/test7800/hardware"
 	"github.com/jetsetilly/test7800/ui"
 )
 
+type Context struct {
+	console    string
+	spec       string
+	rand       *rand.Rand
+	Breaks     []error
+	useOverlay bool
+}
+
+func (ctx *Context) Spec() string {
+	return ctx.spec
+}
+
+func (ctx *Context) IsAtari7800() bool {
+	return ctx.console == "7800"
+}
+
+func (ctx *Context) Reset() {
+	ctx.Breaks = ctx.Breaks[:0]
+	ctx.rand = rand.New(rand.NewPCG(0, 0))
+}
+
+func (ctx *Context) Rand8Bit() uint8 {
+	return uint8(ctx.rand.IntN(255))
+}
+
+func (ctx *Context) Rand16Bit() uint16 {
+	return uint16(ctx.rand.IntN(65535))
+}
+
+func (ctx *Context) Break(e error) {
+	ctx.Breaks = append(ctx.Breaks, e)
+}
+
+func (ctx *Context) UseOverlay() bool {
+	return ctx.useOverlay
+}
+
 func main() {
-	ui := ui.NewUI()
-	ctx, err := dbg.Create("7800", "PAL")
-	if err != nil {
-		panic(err)
+	ui := ui.NewUI().WithAudio()
+	ctx := Context{
+		console:    "7800",
+		spec:       "PAL",
+		useOverlay: false,
 	}
+	ctx.Reset()
 	con := hardware.Create(&ctx, ui)
 	ui.UpdateGUI = func() error {
 		fn := con.MARIA.Coords.Frame
