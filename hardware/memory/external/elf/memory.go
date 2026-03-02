JetSetIlly/test7800

commit 3d471529f7229ca7bba3e9e67ce82b26ed3890f2

implemented all suggestions from gopls check on all packages

hardware/memory/external/elf/memory.go
@@ -458,14 +458,10 @@ func (mem *elfMemory) decode(ef *elf.File) error {
 				s := (tgt >> 24) & 0x01
 				j1 := uint32(0)
 				j2 := uint32(0)
-				if t1 == 0x01 {
-					j1 = s ^ 0x00
-				} else {
+				if t1 != 0x01 {
 					j1 = s ^ 0x01
 				}
-				if t2 == 0x01 {
-					j2 = s ^ 0x00
-				} else {
+				if t2 != 0x01 {
 					j2 = s ^ 0x01
 				}
 

hardware/memory/memory.go
@@ -186,7 +186,7 @@ func (mem *Memory) MapAddress(address uint16, read bool) (uint16, Area) {
 	}
 
 	// page 1
-	if address >= 0x0000 && address <= 0x001f {
+	if address <= 0x001f {
 		// INPTCTRL or TIA
 		if mem.INPTCTRL.Lock() {
 			return address, mem.TIA
@@ -353,16 +353,12 @@ func (mem *Memory) MapAddress(address uint16, read bool) (uint16, Area) {
 		return address, mem.External
 	}
 
-	if address >= 0x8000 && address <= 0xffff {
-		if mem.INPTCTRL.BIOS() {
-			return address, mem.BIOS
-		}
-
-		// everything else can be handled by the external package
-		return address, mem.External
+	if mem.INPTCTRL.BIOS() {
+		return address, mem.BIOS
 	}
 
-	panic("unhandled address in memory.MapAddress()")
+	// everything else can be handled by the external package
+	return address, mem.External
 }
 
 // Read memory address as viewed by the CPU. This method of reading creates

gui/ebiten/dialog.go
@@ -27,5 +27,5 @@ func fileRequest(lastSelectedROM string) (string, error) {
 }
 
 func showError(msg string) {
-	dialog.Message(msg).Error()
+	dialog.Message("%s", msg).Error()
 }

hardware/arm/arm.go
@@ -607,9 +607,7 @@ func (arm *ARM) SetInitialRegisters(args ...uint32) error {
 		return fmt.Errorf("ARM7: trying to set registers SP, LR or PC")
 	}
 
-	for i := range args {
-		arm.state.registers[i] = args[i]
-	}
+	copy(arm.state.registers[:], args)
 
 	// fill the pipeline before yielding. this ensures that the PC is
 	// correct on the first call to Run()

commit d689482fdbcaa8e921fd00832bf95e6399ad2ec0

fixed error introduced in 3d471529f

hardware/memory/external/elf/memory.go
@@ -456,8 +456,8 @@ func (mem *elfMemory) decode(ef *elf.File) error {
 				t1 := (tgt >> 22) & 0x01
 				t2 := (tgt >> 23) & 0x01
 				s := (tgt >> 24) & 0x01
-				j1 := uint32(0)
-				j2 := uint32(0)
+				j1 := s
+				j2 := s
 				if t1 != 0x01 {
 					j1 = s ^ 0x01
 				}